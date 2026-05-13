925d60efa849bc4d6bed751f7e7f1c25b1286d5c
Badre <badrelakel@gmail.com>
2026-05-14 00:23:38 +0200

    add application, service and applicationservice entities

---
+++ mmotors-back/Models/ApplicationServices.cs
@@ -0,0 +1,32 @@
+/*
+    * thise file defines the application service entity
+    * this entity is used to store the selected services for each application
+    * its properties are:
+        int id PK
+        int applicationId FK
+        int serviceId FK
+        boolean isSelected
+        string appliedOverheadType "PERCENTAGE, FIXED_AMOUNT"
+        decimal appliedOverheadValue
+        decimal calculatedOverheadAmount
+        datetime selectedAt
+*/
+
+namespace mmotors_back.Models
+{
+    public class ApplicationService
+    {
+        public int Id { get; set; }
+        public int ApplicationId { get; set; }
+        public int ServiceId { get; set; }
+        public bool IsSelected { get; set; }
+        public OverheadType AppliedOverheadType { get; set; }
+        public decimal AppliedOverheadValue { get; set; }
+        public decimal CalculatedOverheadAmount { get; set; }
+        public DateTime? SelectedAt { get; set; }
+
+        // Navigation properties
+        public Application Application { get; set; } = null!;
+        public Service Service { get; set; } = null!;
+    }
+}
