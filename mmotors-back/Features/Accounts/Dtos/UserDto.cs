146dc112482d259ece98f2ea4ddf2a5121f1ccbe
Badre <badrelakel@gmail.com>
2026-05-19 19:29:27 +0200

    cleanup accounts related dtos

--- mmotors-back/Features/Accounts/Dtos/UserDto.cs
+++ mmotors-back/Features/Accounts/Dtos/UserDto.cs
@@ -3,16 +3,16 @@ namespace mmotors_back.Features.Accounts.Dtos
 {
 	public class UserDto
 	{
-		public string Id { get; set; }
-		public DateOnly Created { get; set; }
-		public string Email { get; set; }
-		public string Name { get; set; }
-		public string LastName { get; set; }
+		public required string Id { get; set; }
+		public required DateOnly Created { get; set; }
+		public required string Email { get; set; }
+		public required string Name { get; set; }
+		public required string LastName { get; set; }
 	}
 
 	public class VerifyTokenDto
 	{
-		public string Email { get; set; }
+		public required string Email { get; set; }
 		public int Token { get; set; }
 	}
 }
